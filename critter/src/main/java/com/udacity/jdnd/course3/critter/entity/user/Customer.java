ec992358b9e692eed88aef66df7e76582c6fe4d3
Foystor <foystor@gmail.com>
2022-08-22 07:47:57 +0800

    fix: Use identity id

--- critter/src/main/java/com/udacity/jdnd/course3/critter/entity/user/Customer.java
+++ critter/src/main/java/com/udacity/jdnd/course3/critter/entity/user/Customer.java
@@ -1,14 +1,20 @@
 package com.udacity.jdnd.course3.critter.entity.user;
 
 import com.udacity.jdnd.course3.critter.entity.pet.Pet;
+import org.hibernate.annotations.Nationalized;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.OneToMany;
+import javax.persistence.*;
 import java.util.List;
 
 @Entity
-public class Customer extends User {
+public class Customer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Nationalized
+    private String name;
 
     @Column(name = "phone")
     private String phoneNumber;
@@ -18,6 +24,22 @@ public class Customer extends User {
 
     @OneToMany(mappedBy = "owner")
     private List<Pet> pets;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 
     public String getPhoneNumber() {
         return phoneNumber;
--- critter/src/main/java/com/udacity/jdnd/course3/critter/entity/user/Employee.java
+++ critter/src/main/java/com/udacity/jdnd/course3/critter/entity/user/Employee.java
@@ -1,11 +1,20 @@
 package com.udacity.jdnd.course3.critter.entity.user;
 
+import org.hibernate.annotations.Nationalized;
+
 import javax.persistence.*;
 import java.time.DayOfWeek;
 import java.util.Set;
 
 @Entity
-public class Employee extends User {
+public class Employee {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Nationalized
+    private String name;
 
     @ElementCollection(targetClass = EmployeeSkill.class)
     @Enumerated(EnumType.STRING)
@@ -17,6 +26,22 @@ public class Employee extends User {
     @CollectionTable(name = "employee_day")
     @Column(name = "day")
     private Set<DayOfWeek> daysAvailable;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 
     public Set<EmployeeSkill> getSkills() {
         return skills;
